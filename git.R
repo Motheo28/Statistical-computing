7fd77c59f03901fd249130b0f3bc808fb0b731e6
Motheo Mokgabudi <MKGMOT018@myuct.ac.za>
2025-02-05 10:08:01 +0200

    Initial commit

--- git.R
+++ git.R
@@ -1,6 +1,11 @@
 if (!requireNamespace("usethis", quietly = TRUE)) {
   install.packages("usethis")
 }
-
-gert::git_config_global_set("user.name", "Motheo Mokgabudi")
-gert::git_config_global_set("user.email", "MKGMOT018@myuct.ac.za")
+gert::git_config_global()
+username <- "Motheo Mokgabudi"
+email <- "MKGMOT018@myuct.ac.za" 
+stopifnot(!is.null(username))
+stopifnot(!is.null(email))
+gert::git_config_global_set("user.name",  username)
+gert::git_config_global_set("user.email", email)
+usethis::use_git()
